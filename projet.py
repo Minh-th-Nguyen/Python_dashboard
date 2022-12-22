8472ed9539e6823596fe0ee67fd7ea686c17bf66
Main PC <nvtnl@DESKTOP-GMCMGFB>
2022-12-22 17:29:43 +0100

    Corrected bug in histograms where bin size were always equal to bin size of 2021

--- projet.py
+++ projet.py
@@ -9,7 +9,7 @@ import geopandas as gpd
 #import data
 data = pd.read_csv('Data.csv', sep = ';')
 for i in range(2019,2022) :
-    data[data['rentree_scolaire'] == i].hist(column = 'nombre_total_eleves', bins = int(data[data['rentree_scolaire'] == 2021]['nombre_total_eleves'].max()), grid=False, figsize=(12,8), color='#86bf91', zorder=2, rwidth=0.9)
+    data[data['rentree_scolaire'] == i].hist(column = 'nombre_total_eleves', bins = data[data['rentree_scolaire'] == i]['nombre_total_eleves'].max(), grid=False, figsize=(12,8), color='#86bf91', zorder=2, rwidth=0.9)
     print(data[data['rentree_scolaire'] == i]['nombre_total_eleves'].sum())
 plt.show()
 
